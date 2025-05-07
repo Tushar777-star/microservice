6a4711aa85ad52e679a66b10277c30970c474bf1
Tushar777-star <tusharabes1095@gmail.com>
2025-05-07 22:57:34 +0530

    Commit Manage Employees Microservice

---
+++ ManageEmployees/ManageEmployees/src/test/java/com/example/training/ManageEmployees/ManageEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.training.ManageEmployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
